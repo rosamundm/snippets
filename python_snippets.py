55bf993d3123a57bcc7adf300fbe82e0201b9776
Rosamund <rsmnd.mthr@gmail.com>
2020-08-12 20:23:06 +0200

    updated snippets

--- python_snippets.py
+++ python_snippets.py
@@ -2,3 +2,5 @@
 import keyword
 keyword.iskeyword("keyword") # returns Boolean value
 
+# check paths that Django is searching for a static file:
+python manage.py findstatic --verbosity 2 <filepath>
