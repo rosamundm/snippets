2b3304f10e2c51135b01b27f6e9510654214392c
Rosamund Mather <rsmnd.mthr@gmail.com>
2020-11-22 19:49:12 +0100

    add pip tip

--- python_snippets.py
+++ python_snippets.py
@@ -4,3 +4,6 @@ keyword.iskeyword("keyword") # returns Boolean value
 
 # check paths that Django is searching for a static file:
 python manage.py findstatic --verbosity 2 <filepath>
+
+# install package into specific non-default directory:
+pip install --target=/my/directory/name/ <package>
