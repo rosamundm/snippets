23c92cbe5edd5300fb80e1007fbac97140d21731
Rosamund Mather <rsmnd.mthr@gmail.com>
2020-12-23 13:15:44 +0100

    add more python snippets

--- python_snippets.py
+++ python_snippets.py
@@ -2,8 +2,24 @@
 import keyword
 keyword.iskeyword("keyword") # returns Boolean value
 
+
 # check paths that Django is searching for a static file:
 python manage.py findstatic --verbosity 2 <filepath>
 
+
 # install package into specific non-default directory:
 pip install --target=/my/directory/name/ <package>
+
+
+# print elements of a list, one per line:
+print(("\n").join(mylist))
+
+
+# open and print contents of a file:
+with open("my_file.txt") as my_file:
+  my_file_contents = my_file.read()
+print(my_file_contents)
+
+
+# run a package if installed, but not on PATH:
+python3 -m <package> <directory>
