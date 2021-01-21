2b65ef95ca6533b5ceae0f8d659d68c9575a10d8
Rosamund Mather <rsmnd.mthr@gmail.com>
2021-01-21 10:04:13 +0100

    add Django reverse relations snippet

--- python_snippets.py
+++ python_snippets.py
@@ -23,3 +23,8 @@ print(my_file_contents)
 
 # run a package if installed, but not on PATH:
 python3 -m <package> <directory>
+
+
+# get all reverse relations of a Django model,
+# including type of relation:
+[x for x in ModelName._meta.get.fields() if x.auto_created and not x.concrete]
